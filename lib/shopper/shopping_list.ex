50632f12e3344a30ebc852d39322d351426d3d81
Chris Freeman <cfreeman.code@gmail.com>
2016-10-25 09:05:09 -0500

    typespecs for ShoppingList

--- lib/shopper/shopping_list.ex
+++ lib/shopper/shopping_list.ex
@@ -3,20 +3,25 @@ defmodule Shopper.ShoppingList do
   @moduledoc """
   The data structure for our shopping list
   """
-  defstruct [count: 0, items: %MapSet{}]
+  @type t :: %ShoppingList{count: integer, items: MapSet.t}
+  defstruct count: 0, items: %MapSet{}
 
+  @spec new([String.t]) :: ShoppingList.t
   def new(items) when is_list(items) do
     %ShoppingList{items: MapSet.new(items), count: length(items)}
   end
 
+  @spec new(String.t) :: ShoppingList.t
   def new(item) when is_binary(item) do
     %ShoppingList{items: MapSet.new([item]), count: 1}
   end
 
+  @spec new() :: ShoppingList.t
   def new() do
     %ShoppingList{}
   end
 
+  @spec add(ShoppingList.t, String.t) :: ShoppingList.t
   def add(list = %ShoppingList{items: items}, new_item) when is_binary(new_item) do
     new_list = items
     |> MapSet.put(new_item)
@@ -24,6 +29,7 @@ defmodule Shopper.ShoppingList do
     %{list | items: new_list, count: MapSet.size(new_list)}
   end
 
+  @spec add(ShoppingList.t, [String.t]) :: ShoppingList.t
   def add(list = %ShoppingList{items: items}, new_items) when is_list(new_items) do
     new_list = items
     |> MapSet.union(MapSet.new(new_items))
@@ -31,13 +37,17 @@ defmodule Shopper.ShoppingList do
     %{list | items: new_list, count: MapSet.size(new_list)}
   end
 
-  def info(list = %ShoppingList{items: items}) do
+  @spec info(ShoppingList.t) :: String.t
+  def info(list = %ShoppingList{}) do
     list
     |> print
   end
 
+  @spec print(ShoppingList.t) :: String.t
   defp print(%ShoppingList{items: items}) do
-    items
-    |> Enum.map_join("\n", fn(item) -> "- #{item}" end)
+    case Enum.empty?(items) do
+      true -> "There are no items in your shopping list!"
+      false -> items |> Enum.map_join("\n", fn(item) -> "- #{item}" end)
+    end
   end
 end
